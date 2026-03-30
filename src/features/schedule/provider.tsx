3ba107647a897c6471d8426657d906502509b441
acherkashin <a.cherkashin@notamedia.com.ru>
2026-03-31 00:34:46 +0300

    TRE-18: тостеры для событий в расписании

--- src/features/schedule/provider.tsx
+++ src/features/schedule/provider.tsx
@@ -14,6 +14,7 @@ import type {
   ScheduleTimeContextType,
 } from './types'
 
+import { format } from 'date-fns'
 import React, { useCallback, useMemo, useRef, useState } from 'react'
 import { toast } from 'sonner'
 
@@ -31,7 +32,11 @@ import {
   ScheduleStoreContext,
   ScheduleTimeContext,
 } from './context'
-import { deserializeEvent, getScheduleHours } from './helpers'
+import {
+  combineDateAndTime,
+  deserializeEvent,
+  getScheduleHours,
+} from './helpers'
 import { Store } from './store'
 
 export const ScheduleProvider: React.FC<
@@ -70,7 +75,19 @@ export const ScheduleProvider: React.FC<
       const { dto, prevData } = params
       const { id } = dto
       try {
+        const successToastId = toast('Сохранение...')
         await onChange(id, deserializeEvent(dto))
+        toast.dismiss(successToastId)
+        const isSameDate = dto.date === prevData.date
+        const isSameTime =
+          isSameDate && dto.start_time === prevData.start_time
+        if (isSameTime) {
+          toast('Мероприятие сохранено')
+        } else {
+          toast(
+            `Новое время мероприятия: ${format(combineDateAndTime(dto.date, dto.start_time), 'd MMM, H:mm').replace('.', '')}`
+          )
+        }
       } catch {
         store.updateEvent(id, prevData)
         toast('Не удалось изменить мероприятие')
@@ -85,7 +102,10 @@ export const ScheduleProvider: React.FC<
         if (type === 'create') {
           store.deleteEvent(id)
         } else {
+          const successToastId = toast('Удаление...')
           await onDelete(id)
+          toast.dismiss(successToastId)
+          toast('Мероприятие удалено')
         }
       } catch {
         toast('Не удалось удалить мероприятие')
@@ -97,8 +117,11 @@ export const ScheduleProvider: React.FC<
   const onCreateHandler = useCallback(
     async (dto: ScheduleEventRead) => {
       try {
+        const successToastId = toast('Сохранение...')
         await onCreate(deserializeEvent(dto))
         store.deleteEvent(dto.id)
+        toast.dismiss(successToastId)
+        toast('Мероприятие сохранено')
       } catch {
         toast('Не удалось создать мероприятие')
       }
@@ -114,6 +137,7 @@ export const ScheduleProvider: React.FC<
       { from, to }: PeriodRange
     ) => {
       try {
+        const successToastId = toast('Сохранение...')
         await onCreatePeriod(
           {
             ...deserializeEvent(dto),
@@ -123,6 +147,8 @@ export const ScheduleProvider: React.FC<
           period
         )
         store.deleteEvent(id)
+        toast.dismiss(successToastId)
+        toast('Мероприятие сохранено')
       } catch {
         toast('Не удалось создать мероприятия с периодом')
       }
@@ -137,7 +163,10 @@ export const ScheduleProvider: React.FC<
       prevData: ScheduleEvent
     ) => {
       try {
+        const successToastId = toast('Сохранение...')
         await onChangePeriod(periodId)
+        toast.dismiss(successToastId)
+        toast('Мероприятие сохранено')
       } catch {
         store.updateEvent(eventId, prevData)
         toast('Не удалось изменить период')
